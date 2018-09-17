70b8e78cbb16821210aa86ce3cb8c93bd620dfe8
James Lane <hello@jameslane.co.uk>
2018-09-17 17:03:14 +0100

    File restructure. Added package.json. Added nunjucks. Added new pages.

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,27 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+var nunjucksRender = require('gulp-nunjucks-render');
  
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('build/css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('src/sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', [ 'sass' ]);
+gulp.task('nunjucks', function() {
+  return gulp.src('src/templates/**/*.+(html|njk)')
+  .pipe(nunjucksRender({
+      path: ['src/templates']
+    }))
+  .pipe(gulp.dest('build'))
+});
+
+gulp.task('default', [ 'sass', 'nunjucks' ]);
