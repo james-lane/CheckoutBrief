7cf68afc5e351821a68d8f7d57a09d75a54fff28
James Lane <james.lane@jdwilliams.co.uk>
2018-09-19 11:55:09 +0100

    Added a bit of ES6 to payment options. Fixed typo in billing intro text. Added min-height to payment options to stop height changing.

--- build/scripts.js
+++ build/scripts.js
@@ -1,4 +1,17 @@
 function matalanCheckout(){
-    //
+    let paymentOptions = document.querySelectorAll('.payment-options a');
+
+    function deactivateOthers(elems) {
+        elems.forEach((elem) => {
+            elem.classList.remove('active');
+        });
+    }
+
+    paymentOptions.forEach((paymentOption) => {
+        paymentOption.addEventListener('click', function() {
+            deactivateOthers(paymentOptions, paymentOption);
+            paymentOption.classList.add('active');
+        });
+    });
 }
 matalanCheckout();
